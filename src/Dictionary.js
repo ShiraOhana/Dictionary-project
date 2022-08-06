1b7703d66c5051ef25cb0f10e6ae73937b58fcda
ShiraOhana <soshiraohana@gmail.com>
2022-08-06 19:06:41 +0300

    Added API and axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
-  return (
+
+  //api.dictionaryapi.dev/api/v2/entries/en/sunset
+  https: return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
